ed32ba05b477996b13e4d0b0d7cbfc6ce16a09a3
Beka Westberg <bekawestberg@gmail.com>
2026-01-03 13:00:06 -0800

    polish: add shadow to pot

--- app/components/Pot.tsx
+++ app/components/Pot.tsx
@@ -37,41 +37,53 @@ export default function Pot({
   };
 
   return (
-    <button
-      onClick={handleClick}
-      disabled={!canSelect}
-      className={`
-        relative
-        w-full aspect-16/23
-        bg-transparent
-        border-none
-        p-0s
-        [--outline-color:theme(colors.rust.300)]
-        ${isSelected ? "outline-stroke" : ""}
-        ${!canSelect ? "cursor-not-allowed opacity-50" : "cursor-pointer"}
-      `}
-      aria-label={isEmpty ? "Empty pot" : "Pot with plant"}
-      aria-pressed={isSelected}
-    >
+    <div className="relative w-full aspect-16/23">
       <svg
         viewBox="0 0 106 105"
-        className="absolute bottom-0 w-full"
+        className="absolute bottom-0 w-full pointer-events-none"
         aria-hidden="true"
       >
         <path
-          style={{
-            fill: hasMutagen ? "#927629" : "var(--color-rust-700)",
-          }}
-          d="M 26.61,58.67 42.1,54.30 H 60.41 L 74.77,58.53 61.25,63.32 38.86,63.18 Z"
-        />
-        <path
-          style={{ fill: "var(--color-stone-700)" }}
-          d="m 24.5,59.80 13.94,5.49 23.66,0.14 15.63,-6.34 1.97,11.41 L 77.16,86.83 65.34,105 37.36,104.97 24.36,86.55 22.25,69.09 Z"
+          id="shadow"
+          style={{ fill: "hsl(var(--shadow-color) / 0.5)" }}
+          d="M 65.23,106.72 87.01,94.49 92.84,78.89 74.28,75.72 Z"
         />
       </svg>
-      <div className="absolute top-0 left-0 z-10 w-full h-full">
-        {isEmpty ? null : children}
-      </div>
-    </button>
+      <button
+        onClick={handleClick}
+        disabled={!canSelect}
+        className={`
+          absolute inset-0
+          bg-transparent
+          border-none
+          p-0s
+          [--outline-color:theme(colors.rust.300)]
+          ${isSelected ? "outline-stroke" : ""}
+          ${!canSelect ? "cursor-not-allowed opacity-50" : "cursor-pointer"}
+        `}
+        aria-label={isEmpty ? "Empty pot" : "Pot with plant"}
+        aria-pressed={isSelected}
+      >
+        <svg
+          viewBox="0 0 106 105"
+          className="absolute bottom-0 w-full"
+          aria-hidden="true"
+        >
+          <path
+            style={{
+              fill: hasMutagen ? "#927629" : "var(--color-rust-700)",
+            }}
+            d="M 26.61,58.67 42.1,54.30 H 60.41 L 74.77,58.53 61.25,63.32 38.86,63.18 Z"
+          />
+          <path
+            style={{ fill: "var(--color-stone-700)" }}
+            d="m 24.5,59.80 13.94,5.49 23.66,0.14 15.63,-6.34 1.97,11.41 L 77.16,86.83 65.34,105 37.36,104.97 24.36,86.55 22.25,69.09 Z"
+          />
+        </svg>
+        <div className="absolute top-0 left-0 z-10 w-full h-full">
+          {isEmpty ? null : children}
+        </div>
+      </button>
+    </div>
   );
 }
