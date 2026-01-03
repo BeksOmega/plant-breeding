8fc1cb680817a03a51a86638536e2f9243005038
Beka Westberg <bekawestberg@gmail.com>
2026-01-02 16:41:02 -0800

    feat: add basic seed type

---
+++ app/types/seed.ts
@@ -0,0 +1,42 @@
+import { PlantGenetics } from "./genetics";
+
+/**
+ * Enum representing the different types of plants that can be grown from seeds.
+ */
+export enum PlantType {
+  ShepherdsSpindel = "ShepherdsSpindel",
+}
+
+/**
+ * Represents a seed that can be planted to grow a specific type of plant.
+ * Contains the plant's genome and the type of plant it will grow into.
+ */
+export class Seed {
+  /**
+   * The genetic information of the plant that will grow from this seed.
+   */
+  readonly genome: PlantGenetics;
+
+  /**
+   * The type of plant this seed will grow into.
+   */
+  readonly plantType: PlantType;
+
+  constructor(genome: PlantGenetics, plantType: PlantType) {
+    this.genome = genome;
+    this.plantType = plantType;
+  }
+
+  /**
+   * Creates a copy of this seed.
+   */
+  clone(): Seed {
+    return new Seed(
+      {
+        chromosome1: [...this.genome.chromosome1],
+        chromosome2: [...this.genome.chromosome2],
+      },
+      this.plantType
+    );
+  }
+}
